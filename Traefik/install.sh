64b291c7c0c20a01bffcf0d729b2ce1348c819f6
XenoBIT78 <xenobit78@gmail.com>
2025-02-21 03:41:47 +0100

    Update install.sh

--- Traefik/install.sh
+++ Traefik/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -vO https://raw.githubusercontent.com/XenoBIT78/docker/refs/heads/main/Traefik/docker-compose.yml
+curl -sO https://raw.githubusercontent.com/XenoBIT78/docker/refs/heads/main/Traefik/docker-compose.yml
 echo "done downloading files"
